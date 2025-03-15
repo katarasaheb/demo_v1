7d97744780d25bfd509d8a4326c538589a85f41d
katarasaheb <cafebymistake00@gmail.com>
2025-03-15 02:40:32 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,3 @@
-// Toggle mobile navigation menu
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
 
 // Form Popup Handling
 const joinBtn = document.getElementById('join-revolution-btn');
