4cb4842b0cccf0dc151d8eb156348bf4b9f58064
katarasaheb <cafebymistake00@gmail.com>
2025-03-17 00:39:28 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@ function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({behavior: 'smooth'});
 }
 
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
