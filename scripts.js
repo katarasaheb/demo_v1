fabf2c22c6b5569179835a6b30281c3e4f65349a
katarasaheb <cafebymistake00@gmail.com>
2025-03-17 00:57:37 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
+// Scroll to section function with smooth scrolling
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
+
     if (section) {
-        section.scrollIntoView({ behavior: "smooth" });
+        window.scrollTo({
+            top: section.offsetTop - 50,  // Adjusting for header offset
+            behavior: 'smooth'
+        });
     }
 }
