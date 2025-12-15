7801913b208e3ad3978b64c836e82b486f178fec
katarasaheb <cafebymistake00@gmail.com>
2025-12-15 01:30:37 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -87,3 +87,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+
+  /* Sticky nav scroll effect */
+  const nav = document.querySelector(".nav");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 40) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
+
+});
